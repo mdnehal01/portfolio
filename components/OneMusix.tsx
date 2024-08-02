2465dab464dc97ca96e205df596a591ef4bfcaaf
mdnehal01 <mdnehalakhlaque43@gmail.com>
2024-08-02 18:02:26 +0530

    NEAE

--- components/OneMusix.tsx
+++ components/OneMusix.tsx
@@ -4,6 +4,8 @@ import ShimmerButton from './ui/ShimmerButton'
 import { MacbookScroll } from './ui/MacScroll'
 import { FaLocationArrow } from 'react-icons/fa6'
 import { useRouter } from 'next/navigation'
+import { BsGithub } from 'react-icons/bs'
+import { BiLink } from 'react-icons/bi'
 
 const OneMusix = () => {
     const router = useRouter();
@@ -13,8 +15,8 @@ const OneMusix = () => {
                 <MacbookScroll title="Developer and founder of One Musix app." src="/oneMusix.png"/>
             </div>
             <div className='flex justify-center items-center gap-5'>
-                <ShimmerButton handleClick={()=>{router.push('https://onemusix.vercel.app/')}} title="Visit" icon={<FaLocationArrow/>} position='left'/>
-                <ShimmerButton handleClick={()=>{router.push('https://github.com/mdnehal01/musix')}} title="Github" icon={<FaLocationArrow/>} position='left'/>
+                <ShimmerButton handleClick={()=>{router.push('https://onemusix.vercel.app/')}} title="Visit" icon={<BiLink/>} position='left'/>
+                <ShimmerButton handleClick={()=>{router.push('https://github.com/mdnehal01/musix')}} title="Github" icon={<BsGithub/>} position='left'/>
             </div>
         </div>
   )
